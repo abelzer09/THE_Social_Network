784884726559d92be3c4101e2efbe2710e2c04d4
abelzer09 <aaronrbelzer@gmail.com>
2022-05-15 16:37:13 -0500

    made usercontroller and started thoughtContoller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,94 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought, reactionSchema} = require('../models')
+
+const thoughtController = {
+    getThought(req, res){
+        Thought.find()
+        .sort({createdAt: -1})
+        .then((info) =>{
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    getSingleThought(req, res){
+        Thought.findOne({_id: req.params.thoughtId})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    createThought(req, res){
+        Thought.create(req.body)
+        .then((info) =>{
+            return User.findOneAndUpdate(
+                {_id: req.body.userId},
+                {$push: {thoughts: info._id}},
+                {new: true}
+            )
+        }).then((data) => {
+        if (!data){
+            return res.status(404)
+        } 
+          res.json(data)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    updateThought(req, res){
+        Thought.findOneAndUpdate({_id: req.params.thoughtId}, {$set: req.body}, {runValidators: true, new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    deleteThought(req, res){
+        Thought.findOneAndDelete({_id: req.params.thoughtId})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            return User.findOneAndUpdate( {_id: req.body.thoughId},
+                {$pull: {thoughts: req.params.thoughtId}},
+                {new: true})
+        }).then(() =>
+        res.json({message: "User can't remember thought"})).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    addFriend(req, res){
+        User.findOneAndUpdate({_id: req.params.userId}, {$addToSet: {friends: req.params.friendId}}, {new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        });
+    },
+    deleteFriend(req, res){
+        User.findOneAndUpdate({_id: req.params.userId}, {$pull: {friends: req.params.friendId}}, {new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,85 @@
 const { ObjectId } = require('mongoose').Types;
+const { User, Thought, reactionSchema} = require('../models')
+
+const userController = {
+    getUsers(req, res){
+        User.find()
+        .then((info) =>{
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    getSingleUser(req, res){
+        User.findOne({_id: req.params.userId})
+        .populate("friends")
+        .populate("thoughts")
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    createUser(req, res){
+        User.create(req.body)
+        .then((info) =>{
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    updateUser(req, res){
+        User.findOneAndUpdate({_id: req.params.userId}, {$set: req.body}, {runValidators: true, new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    deleteUser(req, res){
+        User.findOneAndDelete({_id: req.params.userId})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            return Thought.deleteMany( {_id: {$in: info.thoughts} } )
+        }).then(() =>
+        res.json({message: "User can't remember thought"})).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+    addFriend(req, res){
+        User.findOneAndUpdate({_id: req.params.userId}, {$addToSet: {friends: req.params.friendId}}, {new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        });
+    },
+    deleteFriend(req, res){
+        User.findOneAndUpdate({_id: req.params.userId}, {$pull: {friends: req.params.friendId}}, {new: true})
+        .then((info) =>{
+            if (!info){
+              return res.status(404)
+            } 
+            res.json(info)
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+    },
+};
+
+module.exports = userController;
